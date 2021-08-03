78701e3e792225773dc19f9ea1e3f16f8edb4998
Leonardo León <61639703+leonardo-leon@users.noreply.github.com>
2021-08-03 16:47:47 -0500

    Accesos 2

--- app.py
+++ app.py
@@ -83,6 +83,7 @@ def eliminarUsuario():
 def getUser():
     usuarios = Usuarios.query.order_by(Usuarios.fecha_creada)
     result = get_usuarios.dump(usuarios)
+    jsonify(result)
     result.headers.add('Access-Control-Allow-Origin', '*')
-    return jsonify(result)
+    return result
 
